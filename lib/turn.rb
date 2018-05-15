dde0a0e6a9c1f11d5ef416089c75477654f046ce
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-15 02:34:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,13 @@
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0, 8)
-    true
-  end
+  true if position_taken?(board, index) == false && index.between?(0, 8)
 end
 
 def position_taken?(board, index)
@@ -20,7 +18,7 @@ def position_taken?(board, index)
   end
 end
 
-def move(board, index, value = "X")
+def move(board, index, value = 'X')
   board[index] = value
 end
 
@@ -29,7 +27,7 @@ def input_to_index(input)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
+  puts 'Please enter 1-9:'
   input = gets.strip
   index = input_to_index(input)
 
@@ -38,5 +36,4 @@ def turn(board)
     display_board(board)
   else turn(board)
   end
-
 end
